a3498a8f463f124e6e67975afb5ef01cfb0f5f58
DELL <DELL@DESKTOP-4CELK2Q>
2023-06-08 22:18:34 -0600

    Se agrego log y teclado al login

--- src/main/java/views/VLogin.java
+++ src/main/java/views/VLogin.java
@@ -27,44 +27,176 @@ public class VLogin extends javax.swing.JFrame {
     // Listas de usuarios
 
     CListas listas = null;
-
+    private String cadenaNumeros ="";
+    
     public VLogin(CListas listas) {
         initComponents();
         this.listas = listas;
+        
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel3 = new javax.swing.JPanel();
         tfUserName = new javax.swing.JTextField();
-        tfUserPass = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        btn1 = new javax.swing.JButton();
+        btn2 = new javax.swing.JButton();
+        btn3 = new javax.swing.JButton();
+        btn4 = new javax.swing.JButton();
+        btn5 = new javax.swing.JButton();
+        btn6 = new javax.swing.JButton();
+        btn7 = new javax.swing.JButton();
+        btn8 = new javax.swing.JButton();
+        btn9 = new javax.swing.JButton();
+        btn0 = new javax.swing.JButton();
+        btnEntrar = new javax.swing.JButton();
+        btnBackspace = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jPanel3.setBackground(new java.awt.Color(215, 115, 17));
+        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        tfUserName.setFont(new java.awt.Font("Dialog", 0, 24)); // NOI18N
         tfUserName.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 tfUserNameActionPerformed(evt);
             }
         });
-        getContentPane().add(tfUserName, new org.netbeans.lib.awtextra.AbsoluteConstraints(226, 62, 89, -1));
+        jPanel3.add(tfUserName, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 40, 180, -1));
 
-        tfUserPass.addActionListener(new java.awt.event.ActionListener() {
+        jPanel1.setLayout(new java.awt.GridLayout(4, 3));
+
+        btn1.setText("1");
+        btn1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                tfUserPassActionPerformed(evt);
+                btn1ActionPerformed(evt);
             }
         });
-        getContentPane().add(tfUserPass, new org.netbeans.lib.awtextra.AbsoluteConstraints(226, 113, 89, -1));
+        jPanel1.add(btn1);
 
-        jButton1.setText("Entrar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btn2.setText("2");
+        btn2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btn2ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(231, 161, -1, -1));
+        jPanel1.add(btn2);
+
+        btn3.setText("3");
+        btn3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn3);
+
+        btn4.setText("4");
+        btn4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn4ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn4);
+
+        btn5.setText("5");
+        btn5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn5ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn5);
+
+        btn6.setText("6");
+        btn6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn6ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn6);
+
+        btn7.setText("7");
+        btn7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn7ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn7);
+
+        btn8.setText("8");
+        btn8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn8ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn8);
+
+        btn9.setText("9");
+        btn9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn9ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn9);
+
+        btn0.setText("0");
+        btn0.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn0ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn0);
+
+        btnEntrar.setText("Entrar");
+        btnEntrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEntrarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnEntrar);
+
+        btnBackspace.setText("Borrar");
+        btnBackspace.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBackspaceActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnBackspace);
+
+        jPanel3.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(46, 106, 190, 260));
+
+        getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 0, 283, 473));
+
+        jPanel2.setForeground(new java.awt.Color(204, 204, 204));
+
+        jLabel1.setBackground(new java.awt.Color(204, 204, 204));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\DELL\\Documents\\NetBeansProjects\\CasaDoloresProject\\src\\main\\java\\img\\logo.png")); // NOI18N
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(47, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 384, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 356, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(78, Short.MAX_VALUE))
+        );
+
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 460, 473));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -73,13 +205,9 @@ public class VLogin extends javax.swing.JFrame {
 
     }//GEN-LAST:event_tfUserNameActionPerformed
 
-    private void tfUserPassActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tfUserPassActionPerformed
-
-    }//GEN-LAST:event_tfUserPassActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnEntrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEntrarActionPerformed
         String username = tfUserName.getText();
-        String password = tfUserPass.getText();
+        //String password = tfUserPass.getText();
         int index = 0;
         switch (username.substring(0, 1)) {
             case "1" -> {
@@ -92,7 +220,7 @@ public class VLogin extends javax.swing.JFrame {
             case "3" -> {
                 index = listas.BuscarIdMesero(Integer.parseInt(username));
                 new VMesasComedor(listas, index).setVisible(true);
-                
+                System.out.println("entro");
                 // Para Lia
             }
             case "4" -> {
@@ -106,11 +234,136 @@ public class VLogin extends javax.swing.JFrame {
         this.dispose();
 
 
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnEntrarActionPerformed
+
+    private void btn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn1ActionPerformed
+         if(tfUserName.getText().isEmpty()){
+             cadenaNumeros = "1";
+         }else{
+             cadenaNumeros += "1";
+         }
+         
+         tfUserName.setText(cadenaNumeros);
+    }//GEN-LAST:event_btn1ActionPerformed
+
+    private void btn2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn2ActionPerformed
+             if(tfUserName.getText().isEmpty()){
+             cadenaNumeros = "2";
+         }else{
+             cadenaNumeros += "2";
+         }
+         
+         tfUserName.setText(cadenaNumeros);
+    }//GEN-LAST:event_btn2ActionPerformed
+
+    private void btnBackspaceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackspaceActionPerformed
+        String bt;
+        
+        if(tfUserName.getText().length()>0){
+            StringBuilder st = new StringBuilder(tfUserName.getText());
+            st.deleteCharAt(tfUserName.getText().length()-1);
+            bt = st.toString();
+            tfUserName.setText(bt);
+        }
+    }//GEN-LAST:event_btnBackspaceActionPerformed
+
+    private void btn3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn3ActionPerformed
+        if(tfUserName.getText().isEmpty()){
+             cadenaNumeros = "3";
+         }else{
+             cadenaNumeros += "3";
+         }
+         
+         tfUserName.setText(cadenaNumeros);
+    }//GEN-LAST:event_btn3ActionPerformed
+
+    private void btn4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn4ActionPerformed
+         if(tfUserName.getText().isEmpty()){
+             cadenaNumeros = "4";
+         }else{
+             cadenaNumeros += "4";
+         }
+         
+         tfUserName.setText(cadenaNumeros);
+    }//GEN-LAST:event_btn4ActionPerformed
+
+    private void btn5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn5ActionPerformed
+         if(tfUserName.getText().isEmpty()){
+             cadenaNumeros = "5";
+         }else{
+             cadenaNumeros += "5";
+         }
+         
+         tfUserName.setText(cadenaNumeros);
+    }//GEN-LAST:event_btn5ActionPerformed
+
+    private void btn6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn6ActionPerformed
+         if(tfUserName.getText().isEmpty()){
+             cadenaNumeros = "6";
+         }else{
+             cadenaNumeros += "6";
+         }
+         
+         tfUserName.setText(cadenaNumeros);
+    }//GEN-LAST:event_btn6ActionPerformed
+
+    private void btn7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn7ActionPerformed
+         if(tfUserName.getText().isEmpty()){
+             cadenaNumeros = "7";
+         }else{
+             cadenaNumeros += "7";
+         }
+         
+         tfUserName.setText(cadenaNumeros);
+    }//GEN-LAST:event_btn7ActionPerformed
+
+    private void btn8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn8ActionPerformed
+         if(tfUserName.getText().isEmpty()){
+             cadenaNumeros = "8";
+         }else{
+             cadenaNumeros += "8";
+         }
+         
+         tfUserName.setText(cadenaNumeros);
+    }//GEN-LAST:event_btn8ActionPerformed
+
+    private void btn9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn9ActionPerformed
+         if(tfUserName.getText().isEmpty()){
+             cadenaNumeros = "9";
+         }else{
+             cadenaNumeros += "9";
+         }
+         
+         tfUserName.setText(cadenaNumeros);
+    }//GEN-LAST:event_btn9ActionPerformed
+
+    private void btn0ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn0ActionPerformed
+         if(tfUserName.getText().isEmpty()){
+             cadenaNumeros = "0";
+         }else{
+             cadenaNumeros += "0";
+         }
+         
+         tfUserName.setText(cadenaNumeros);
+    }//GEN-LAST:event_btn0ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btn0;
+    private javax.swing.JButton btn1;
+    private javax.swing.JButton btn2;
+    private javax.swing.JButton btn3;
+    private javax.swing.JButton btn4;
+    private javax.swing.JButton btn5;
+    private javax.swing.JButton btn6;
+    private javax.swing.JButton btn7;
+    private javax.swing.JButton btn8;
+    private javax.swing.JButton btn9;
+    private javax.swing.JButton btnBackspace;
+    private javax.swing.JButton btnEntrar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JTextField tfUserName;
-    private javax.swing.JTextField tfUserPass;
     // End of variables declaration//GEN-END:variables
 }
